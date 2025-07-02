3716d61de54927ebc36b98e1a660c4e25ef22678
Moon <100405650+chocobubble@users.noreply.github.com>
2025-07-02 19:19:45 +0900

    :bug: Fix: TBN 공식 수정
    - 보간과 변환 과정에서 직교성이 깨지기 때문에 필요

--- Shaders/common.hlsl
+++ Shaders/common.hlsl
@@ -2,13 +2,15 @@ float3x3 GetTBN(float3 normal, float3 tangent)
 {
 	float3 N = normalize(normal);
 	float3 T = normalize(tangent);
-
+	
+	T = normalize(T - dot(T, N) * N);
+	
 	float3 B = cross(N, T);
 
 	return float3x3(T, B, N);
 }
 
-float3 PixelNormalTS(float3 normalSample, float3x3 TBN)
+float3 NormalTSToWS(float3 normalSample, float3x3 TBN)
 {
 	float3 n = normalSample * 2.0f - 1.0f;
 	return normalize(mul(n, TBN));
